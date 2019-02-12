52c12bb9f6d46293d664b68a290d7640b01378a3
Danny Vail <danny_vail@outlook.com>
2019-02-12 17:38:29 -0500

    INITIAL COMMIT Initializing Spring project

---
+++ src/main/java/com/lambdaschool/restdogs/RestdogsApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.restdogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdogsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestdogsApplication.class, args);
+  }
+
+}
+
