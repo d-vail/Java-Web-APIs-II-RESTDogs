9ad6b2628bdcd5bb23363b4243efe52cde566968
Danny Vail <danny_vail@outlook.com>
2019-02-13 04:30:18 -0500

    CREATE DOGCONTROLLER CLASS In DogController.java - Create the Dog Controller to handle all of the dog endpoints - Add a GET endpoint for /dogs to return a listing of all dogs - Add a GET endpoint for /dogs/{id} to return a listing of one dog by id

---
+++ src/main/java/com/lambdaschool/restdogs/dog/DogController.java
@@ -0,0 +1,58 @@
+package com.lambdaschool.restdogs.dog;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+/**
+ * Dog Controller class to handle /dogs endpoints
+ */
+@RestController
+@RequestMapping("/dogs")
+public class DogController {
+  private final DogRepository DOGREPO;
+  private final DogResourceAssembler ASSEMBLER;
+
+  public DogController(DogRepository dogRepo, DogResourceAssembler assembler) {
+    this.DOGREPO = dogRepo;
+    this.ASSEMBLER = assembler;
+  }
+
+  /**
+   * Returns all dog listings.
+   *
+   * @return  A collection of dog resource objects
+   */
+  @GetMapping()
+  public Resources<Resource<Dog>> all() {
+    List<Resource<Dog>> dogs = DOGREPO.findAll().stream()
+            .map(ASSEMBLER::toResource)
+            .collect(Collectors.toList());
+
+    return new Resources<>(dogs,
+            linkTo(methodOn(DogController.class).all()).withSelfRel());
+  }
+
+  /**
+   * Returns a single dog given an id.
+   *
+   * @param id                A dog id
+   * @return                  A dog resource object
+   * @throws RuntimeException If invalid id is given
+   */
+  @GetMapping("/{id}")
+  public Resource<Dog> one(@PathVariable Long id) {
+    Dog dog = DOGREPO.findById(id)
+            .orElseThrow(() -> new DogNotFoundException(id));
+    return ASSEMBLER.toResource(dog);
+  }
+}
